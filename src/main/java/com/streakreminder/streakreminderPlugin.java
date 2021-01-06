8fa86918a8e5c99efc27f7ebc4107a14e40ef652
vincent0955 <74017998+vincent0955@users.noreply.github.com>
2021-01-05 20:51:21 -0500

    init commit
    Works properly now, placeholders still need to be filled

--- src/main/java/com/streakreminder/streakreminderPlugin.java
+++ src/main/java/com/streakreminder/streakreminderPlugin.java
@@ -5,14 +5,14 @@ import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
+import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.ChatMessage;
-import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import java.awt.Color;
+
 
 @Slf4j
 @PluginDescriptor(
@@ -30,20 +30,16 @@ public class streakreminderPlugin extends Plugin
 	private streakreminderConfig config;
 
 	private boolean RemindStatus = false;
+	private String CurrentStreak = null;
 	private String CurrentMessage;
-	private int TasksCompleted;
-	private String streak10 = "Your next slayer task will be a streak of 10";
-	private String streak50 = "Your next slayer task will be a streak of 50";
-	private String streak100 = "Your next slayer task will be a streak of 100";
-	private String streak250 = "Your next slayer task will be a streak of 250";
-	private String streak1000 = "Your next slayer task will be a streak of 1000";
-	private String hex10 = "<col="+Integer.toHexString(config.getStreak10Color().getRGB()).substring(2)+">";
-	private String hex50 = "<col="+Integer.toHexString(config.getStreak50Color().getRGB()).substring(2)+">";
-	private String hex100 = "<col="+Integer.toHexString(config.getStreak100Color().getRGB()).substring(2)+">";
-	private String hex250 = "<col="+Integer.toHexString(config.getStreak250Color().getRGB()).substring(2)+">";
-	private String hex1000 = "<col="+Integer.toHexString(config.getStreak1000Color().getRGB()).substring(2)+">";
-	//private String[] tasksCompstr = new String[] {};
-	// reference: "You've completed 9 tasks and received 15 points, giving you a total of 120; return to a Slayer master."
+	private String streak10 = "Your next slayer task will be a streak of <col=ff0000>10.";
+	private String streak50 = "Your next slayer task will be a streak of <col=ff0000>50.";
+	private String streak100 = "Your next slayer task will be a streak of <col=ff0000>100.";
+	private String streak250 = "Your next slayer task will be a streak of <col=ff0000>250.";
+	private String streak1000 = "Your next slayer task will be a streak of <col=ff0000>1000.";
+	private WorldPoint nieveArea = new WorldPoint(2954,3317, 0);
+	private WorldPoint DuradelArea = new WorldPoint(0,0, 0); //placeholder tiles
+	private WorldPoint TuraelArea = new WorldPoint(0,0, 0);
 
 	@Provides
 	streakreminderConfig provideConfig(ConfigManager configManager)
@@ -52,14 +48,12 @@ public class streakreminderPlugin extends Plugin
 	}
 
 	@Subscribe
-	public void onChatMessage(ChatMessage chatMessage)
-	{
-		if (chatMessage.getType() == ChatMessageType.PUBLICCHAT) {
+	public void onChatMessage(ChatMessage chatMessage) {
+		if (chatMessage.getType().equals(ChatMessageType.PUBLICCHAT)) {
 			CurrentMessage = chatMessage.getMessage();
 			if (CurrentMessage.contains("points, giving you a total of")) {
-
 				//obtain the number of tasks completed from the string
-				String[] tasksCompstr = CurrentMessage.split(" ", 3);
+				String[] tasksCompstr = CurrentMessage.split(" ", 4);
 				int TasksCompleted = Integer.parseInt(tasksCompstr[2]);
 				int lastDigit = TasksCompleted % 10;
 				int last2Digits = TasksCompleted % 100;
@@ -67,42 +61,44 @@ public class streakreminderPlugin extends Plugin
 
 				if (last3Digits == 999){
 					RemindStatus = true;
-					client.addChatMessage(ChatMessageType.GAMEMESSAGE,"", streak1000 + hex1000, "");
+					CurrentStreak = streak1000;
+					client.addChatMessage(ChatMessageType.GAMEMESSAGE,"",streak1000,"");
 				}
 
-				else if (last3Digits == 249 || last3Digits == 499 || last3Digits == 749){
+				else if (last3Digits==249 || last3Digits == 499 || last3Digits == 749){
 					RemindStatus = true;
-					client.addChatMessage(ChatMessageType.GAMEMESSAGE,"", streak250 + hex250, "");
+					CurrentStreak = streak250;
+					client.addChatMessage(ChatMessageType.GAMEMESSAGE,"",streak250,"");
 				}
 
 				else if (last2Digits == 99) {
 					RemindStatus = true;
-					client.addChatMessage(ChatMessageType.GAMEMESSAGE,"", streak100 + hex100, "");
+					CurrentStreak = streak100;
+					client.addChatMessage(ChatMessageType.GAMEMESSAGE,"",streak100,"");
 				}
 
 				else if (last2Digits == 49) {
 					RemindStatus = true;
-					client.addChatMessage(ChatMessageType.GAMEMESSAGE,"", streak50 + hex50, "");
+					CurrentStreak = streak50;
+					client.addChatMessage(ChatMessageType.GAMEMESSAGE,"",streak50,"");
 				}
 
 				else if (lastDigit == 9) {
 					RemindStatus = true;
-					client.addChatMessage(ChatMessageType.GAMEMESSAGE,"", streak10 + hex10, "");
+					CurrentStreak = streak10;
+					client.addChatMessage(ChatMessageType.GAMEMESSAGE,"", streak10,"");
 				}
 			}
 		}
-
 	}
-//	@Override
-//	protected void startUp() throws Exception
-//	{
-//		log.info("Example started!");
-//	}
-//
-//	@Override
-//	protected void shutDown() throws Exception
-//	{
-//		log.info("Example stopped!");
-//	}
-
+	@Subscribe
+	public void onGameTick(GameTick event){
+		if (RemindStatus) {
+			WorldPoint CurrentLocation = client.getLocalPlayer().getWorldLocation();
+			if (CurrentLocation.distanceTo2D(nieveArea) <= config.remindDistance()) {
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE,"", CurrentStreak,"");
+				RemindStatus = false;
+			}
+		}
+	}
 }
